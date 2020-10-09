9ae6a82c1a380732a528a404f1d0a51ceb787d63
Welbert Hime <welberthime@hotmail.com>
2020-10-09 13:05:03 -0300

    parametrize weight file

--- train.bash
+++ train.bash
@@ -7,7 +7,12 @@ fi
 
 data=/content/voc-person-detection/voc.data
 cfg=/content/voc-person-detection/${1}.cfg
-weights=${1}.conv.15
+
+if [ "${2}" = "" ]; then
+    weights=/content/darknet/yolov3-tiny.conv.15
+else
+    weights=/content/darknet/${2}.conv.15
+fi
 
 original_location=$(pwd)
 cd /content/darknet
